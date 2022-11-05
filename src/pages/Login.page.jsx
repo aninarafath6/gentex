562ef83de694aec3393c7f638a937c6328ea2c5b
aninarafath6 <arafathanin6@gmail.com>
2022-11-06 04:12:21 +0530

    all done

--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -7,7 +7,6 @@ import Logo from "../assets/images/logo.png";
 function LoginPage() {
     return (
         <div className='flex'>
-
             <div className='w-5/12 h-screen bg-gray-100 flex items-center justify-center flex-col  '>
                 <img src={Logo} width="180" alt="" className='absolute top-10 left-10' />
                 <img src={LoginImage} width='250' alt="" />
