fdcf6c87ec30130cfb7981d33ab11107e7067c6b
aninarafath6 <arafathanin6@gmail.com>
2022-11-06 04:11:06 +0530

    ui changed

--- src/pages/Login.page.jsx
+++ src/pages/Login.page.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import LoginImage from "../assets/images/login.jpg";
+import LoginImage from "../assets/images/login.png";
+import Logo from "../assets/images/logo.png";
+
 
 
 function LoginPage() {
     return (
         <div className='flex'>
-            <div className='w-5/12 h-screen bg-violet-100 flex items-center justify-center '>
 
-                <img src={LoginImage} alt="" />
-
+            <div className='w-5/12 h-screen bg-gray-100 flex items-center justify-center flex-col  '>
+                <img src={Logo} width="180" alt="" className='absolute top-10 left-10' />
+                <img src={LoginImage} width='250' alt="" />
             </div>
 
 
