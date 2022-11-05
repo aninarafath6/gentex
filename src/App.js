dcea61ff7ff4d551e48a96538abcfcbc94ee408c
MIDLAJ C <mail.midlajc@gmail.com>
2022-11-06 04:21:21 +0530

    404 Update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import NotFound from "./pages/NotFound.page";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
