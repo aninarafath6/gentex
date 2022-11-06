1af7e445c6734f5d5cd5a9631cf20ab1a8182f95
MIDLAJ C <mail.midlajc@gmail.com>
2022-11-06 13:31:49 +0530

    conflic fix

--- src/pages/SuperAdmin.page.jsx
+++ src/pages/SuperAdmin.page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
 
-import SideBar from "../components/Sidebar";
+import SideBar from "../components/SideBar";
 
 function SuperAdminPage() {
   return (
